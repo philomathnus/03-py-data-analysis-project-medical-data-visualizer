ffd2c578f164a444ba4e4aefbd88fd0e42d467ff
u210979 <smadar.klopshtok@sbb.ch>
2025-06-18 11:45:20 +0200

    working cat plot

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+#! .\.venv\scripts\python.exe
+
 # This entrypoint file to be used in development. Start by reading README.md
 import medical_data_visualizer
 from unittest import main
 
 # Test your function by calling it here
 medical_data_visualizer.draw_cat_plot()
-medical_data_visualizer.draw_heat_map()
+#medical_data_visualizer.draw_heat_map()
 
 # Run unit tests automatically
-main(module='test_module', exit=False)
+#main(module='test_module', exit=False)
--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -4,33 +4,33 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # 1
-df = None
+df = pd.read_csv('medical_examination.csv')
 
 # 2
-df['overweight'] = None
+df['bmi'] = (df.weight / np.square(df.height)) * 10000
+df['overweight'] = np.where(df.bmi > 25, 1, 0)
 
 # 3
-
+df['cholesterol'] = np.where(df.cholesterol > 1, 1, 0)
+df['gluc'] = np.where(df.gluc > 1, 1, 0)
+print(df.head())
 
 # 4
 def draw_cat_plot():
-    # 5
-    df_cat = None
-
-
-    # 6
-    df_cat = None
+    # 5 - Create a DataFrame for the cat plot using pd.melt with values from cholesterol, gluc, smoke, alco, active, and overweight
+    df_cat = df.melt(id_vars='cardio', value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'])
     
+    # 6 - Group & reformat: split by cardio, show count of each feature
+    df_cat = df_cat.groupby(['cardio', 'variable', 'value']).size().reset_index()
+    df_cat = df_cat.rename(columns={0: 'total'})
 
-    # 7
+    # 7 - create a chart that shows the value counts of the categorical features using the following method provided by the seaborn library import: sns.catplot().
+    graph = sns.catplot(data=df_cat, kind="bar", x="variable", y="total", hue="value", col="cardio")
 
+    # 8 - Get the figure for the output and store it in the fig variable
+    fig = graph.figure
 
-
-    # 8
-    fig = None
-
-
-    # 9
+    # 9 - save figure to file
     fig.savefig('catplot.png')
     return fig
 
